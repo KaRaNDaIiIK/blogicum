639f2c28d51e256ec7ac3d0e89619fd4e35170cd
Yura Donchev <Donchev.yus@yandex.ru>
2025-04-20 20:20:16 +0300

    Добавлен код в admin

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Post  # Добавляем импорт модели
 
-# Register your models here.
+admin.site.register(Post)  # Регистрируем модель
--- blogicum/pages/admin.py
+++ blogicum/pages/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Post  # Добавляем импорт модели
 
+admin.site.register(Post)  # Регистрируем модель
 # Register your models here.
