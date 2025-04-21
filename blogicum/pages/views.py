8988f034f9aaff3926556c59b3bd6c3caa4087c8
Yura Donchev <Donchev.yus@yandex.ru>
2025-04-21 14:33:48 +0300

    Добавлена пустая строка после docsting

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -3,9 +3,11 @@ from django.views.generic.base import TemplateView
 
 class AboutView(TemplateView):
     """Класс-представление для страницы 'О проекте'."""
+
     template_name = 'pages/about.html'
 
 
 class RulesView(TemplateView):
     """Класс-представление для страницы 'Наши правила'."""
+
     template_name = 'pages/rules.html'
