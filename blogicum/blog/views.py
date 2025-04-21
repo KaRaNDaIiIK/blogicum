1f991193d5ca9d9f3eaedc01cd0f944567f62b72
Yura Donchev <Donchev.yus@yandex.ru>
2025-04-21 12:39:50 +0300

    убраны лишние переменные в views. Переменная ID=post_id

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -48,30 +48,27 @@ posts = [
 
 def index(request: HttpRequest) -> HttpResponse:
     """View-функция для страницы 'Лента записей'."""
-    template = 'blog/index.html'
     reversed_posts = list(reversed(posts))
     context = {'posts': reversed_posts}
-    return render(request, template, context)
+    return render(request, 'blog/index.html', context)
 
 
-def post_detail(request: HttpRequest, id: int) -> HttpResponse:
+def post_detail(request: HttpRequest, post_id: int) -> HttpResponse:
     """View-функция для детального просмотра одной записи."""
     try:
-        post = posts[id]  # Получаем конкретный пост по ID
+        post = posts[post_id]  # Получаем конкретный пост по ID
     except IndexError:
         raise Http404("Запись не найдена")
-    template = 'blog/detail.html'
     context = {'post': post}
-    return render(request, template, context)
+    return render(request, 'blog/detail.html', context)
 
 
 def category_posts(request: HttpRequest, category_slug: str) -> HttpResponse:
     """View-функция для просмотра записей определенной категории."""
-    template = 'blog/category.html'
     filtered_posts = [post for post in posts if post['category']
                       == category_slug]
     context = {
         'category': category_slug,
         'posts': filtered_posts
     }
-    return render(request, template, context)
+    return render(request, 'blog/category.html', context)
--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -4,11 +4,9 @@ from django.http import HttpRequest, HttpResponse
 
 def about(request: HttpRequest) -> HttpResponse:
     """View-функция для страницы 'О проекте'."""
-    template = 'pages/about.html'
-    return render(request, template)
+    return render(request, 'pages/about.html')
 
 
 def rules(request: HttpRequest) -> HttpResponse:
     """View-функция для страницы 'Наши правила'."""
-    template = 'pages/rules.html'
-    return render(request, template)
+    return render(request, 'pages/about.html')
